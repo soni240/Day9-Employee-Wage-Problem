ee6a629004599ffbf276a00f6aeb661db52c8957
soni240 <s.fegade21@gmail.com>
2022-06-24 20:21:29 +0530

    [Gunavanti]using object oriented programming

---
+++ UC10_Manage_Employee_Multiple_Compaines/UC10_Manage_Employee_Multiple_Compaines/EmployeeWage.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace EmployeeMultiple
+{
+    internal class EmployeeWage
+    {
+        public EmployeeWage()
+        {
+
+
+            void ComputeEmpWage()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal void ComputeEmpWage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
